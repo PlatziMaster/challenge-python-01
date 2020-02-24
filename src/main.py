dfbe14852510cfc1c77d00019a076f1efb587928
iraida07 <iraidabarreto07@hotmail.com>
2020-02-24 01:18:10 -0500

    Se resuelve reto del palíndromo

--- src/main.py
+++ src/main.py
@@ -8,14 +8,23 @@ PALINDROMES = [
 
 NOT_PALINDROMES = [
     'Hola como estas',
-    'Platzi'
+    'Platzi',
     'Oscar',
 ]
 
 
 def is_palindrome(palindrome):
-    # Start coding here
-    pass
+    palindrome = palindrome.replace(" ","").lower()
+    reverse = ''.join(reversed(palindrome))
+    #print (palindrome)
+    #print (nospaces)
+    if reverse == palindrome:
+        print (palindrome, 'is palindrome :)')
+        return True
+    else:
+        print (palindrome, 'is not palindrome :(')        
+        return False
+
 
 def validate():
     for palindrome in PALINDROMES:
