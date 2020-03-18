130fc5a9fcbb12a47dedc849653ce8cc932ddb0f
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-18 11:05:32 -0500

    challenge resolved, with more palindromes cases added

--- src/main.py
+++ src/main.py
@@ -4,6 +4,19 @@ PALINDROMES = [
     'Acaso hubo buhos aca',
     'A la catalana banal atacala',
     'Amar da drama',
+    "Mr. Owl ate my metal worm",
+    "Do geese see God?",
+    "Was it a car or a cat I saw?",
+    "Murder for a jar of red rum",
+    "Go hang a salami, I'm a lasagna hog",
+    "Rats live on no evil star",
+    "Live on time, emit no evil",
+    "Step on no pets",
+    'bob',
+    'rayar',
+    'Anita lava la tina',
+    'sometemos',
+    'ojo'
 ]
 
 NOT_PALINDROMES = [
@@ -14,7 +27,14 @@ NOT_PALINDROMES = [
 
 
 def is_palindrome(palindrome):
-    # Start coding here
+    word = palindrome.replace(" ","").replace(".","").replace("'","").replace("?","").lower().replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u")
+    reversed_word = word[::-1]
+
+    if reversed_word == word:
+        return True
+    else:
+        print(palindrome)
+        return False
     pass
 
 def validate():
