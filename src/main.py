2cc19ab7f083f72ab6437d0e8b81437afbde0531
Diego Valdez <dgogo9@hotmail.com>
2020-05-04 19:59:10 -0500

    Completando el test

--- src/main.py
+++ src/main.py
@@ -14,8 +14,12 @@ NOT_PALINDROMES = [
 
 
 def is_palindrome(palindrome):
-    # Start coding here
-    pass
+    word = palindrome.upper()
+    word = word.replace(" ","")
+    if word == word[::-1]:
+        return True
+    else:
+        return False
 
 def validate():
     for palindrome in PALINDROMES:
