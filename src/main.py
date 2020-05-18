76d8f3213758dd2087e30269a0cb34cfb72086ef
Jesus Wisord <wisord.autumn@gmail.com>
2020-05-18 16:04:30 -0500

    Challenge solved

--- src/main.py
+++ src/main.py
@@ -8,14 +8,24 @@ PALINDROMES = [
 
 NOT_PALINDROMES = [
     'Hola como estas',
-    'Platzi'
+    'Platzi',
     'Oscar',
 ]
 
 
 def is_palindrome(palindrome):
-    # Start coding here
-    pass
+    # Limpiamos el string, eliminamos espacios y pasamos a minúsculas
+    word = palindrome.replace(" ","")
+    word = word.lower()
+    # Obtenemos el tamaño del string para iterar únicamente la mitad del string
+    size = len(word)
+    size = int(size/2 + 1)
+    for n in range(0, size):
+        if word[n] != word[-1-n]:
+            break
+    else:
+        return True
+    return False
 
 def validate():
     for palindrome in PALINDROMES:
