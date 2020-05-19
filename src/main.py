3262fc0fdb54a86a4d1e137536d0bc9113b87c72
Antonio <a.guerrero.esp94@gmail.com>
2020-05-18 20:38:27 -0500

    Challenge resolved!

--- src/main.py
+++ src/main.py
@@ -18,10 +18,10 @@ def is_palindrome(palindrome):
     BackwardsPalindrome = newPal[::-1] ## Copy the new variable 'newPal' backwards
 
     if BackwardsPalindrome == newPal: ## If both variables are equals, it´s a palindrome, then return true to continue the first loop 'for' on the function validate().
-        print('     The string \'{}\' is a palindrome'.format(palindrome))
+        print('The string \'{}\' is a palindrome'.format(palindrome))
         return True
     elif BackwardsPalindrome != newPal: ## If 'BackwardsPalindrome' isn't the same as 'newPal', 'newPal' isn't a palindrome, then return false to continue the second loop 'for' on the function validate().
-        print('     The string \'{}\' isn\'t a palindrome'.format(palindrome))
+        print('The string \'{}\' isn\'t a palindrome'.format(palindrome))
         return False
 
 def validate():
