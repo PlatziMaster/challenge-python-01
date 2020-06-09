d9579b32e2954d99c0d58e27711757fe066910d4
EdwardTL <edward.toledo.lopez@hotmail.com>
2020-06-09 01:00:58 -0500

    Reto Completado, el programa reconoce palindromos. Atte. Edward

--- src/main.py
+++ src/main.py
@@ -15,6 +15,42 @@ NOT_PALINDROMES = [
 
 def is_palindrome(palindrome):
     # Start coding here
+    palindrome = palindrome.lower()
+    Fifo = []
+    Lifo = []
+    Slot = 0
+    PalindromeSize = len(palindrome)
+
+    for Slot in range(0, PalindromeSize):
+        SlotCheck = palindrome[Slot]
+        if SlotCheck != " ":
+            Fifo.append(SlotCheck)
+
+    LifoHelp = reversed(Fifo)
+    Error = 0
+    Slot = 0
+
+    for eachSlot in LifoHelp:
+        Lifo.append(eachSlot)
+        if Lifo[Slot] != Fifo[Slot]:
+            Error = 1
+            break
+        Slot += 1
+
+    if Error == 1:
+        return False
+    else:
+        return True
+
+def validate():
+    for palindrome in PALINDROMES:
+        if not is_palindrome(palindrome):
+            return False
+
+    for not_palindrome in NOT_PALINDROMES:
+        if is_palindrome(not_palindrome):
+            return False
+    return True
     pass
 
 def validate():
