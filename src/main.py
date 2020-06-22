8f03850c35716fed088f8e23921c047c7e9cfd8f
Daniel Silva Rueda <bukaro.dany@gmail.com>
2020-06-22 00:03:54 -0500

    I can solved the problem

--- src/main.py
+++ src/main.py
@@ -12,10 +12,18 @@ NOT_PALINDROMES = [
     'Oscar',
 ]
 
-
 def is_palindrome(palindrome):
-    # Start coding here
-    pass
+    palindrome_sin_espacios = palindrome.replace(" ", "")
+    palindrome_alrevez = palindrome_sin_espacios[::-1]
+    igual, aux = 0, 0
+    for ind in range(0, len(palindrome_sin_espacios)):
+        if palindrome_sin_espacios[ind].lower() == palindrome_alrevez[aux].lower():
+            igual += 1
+        aux += 1
+
+    if len(palindrome_sin_espacios) == igual:
+        return True
+        
 
 def validate():
     for palindrome in PALINDROMES:
