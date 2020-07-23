90cc099a52f1495c9b72560eeacac63f0b5915fc
zomars <zomars@me.com>
2020-07-23 17:55:43 -0600

    Completa el challenge de palindromos

--- src/main.py
+++ src/main.py
@@ -14,19 +14,34 @@ NOT_PALINDROMES = [
 
 
 def is_palindrome(palindrome):
+    # Start coding here
     # Si no es un string, tratamos de convertirlo
-    type(palindrome) != 'string':
+    if type(palindrome) != 'string':
         palindrome = str(palindrome)
     # Primero necesito quitar los espacios
-    palindrome = palindrome.replace(' ', '')
+    p = palindrome.replace(' ', '')
     # Despues necesito convertir el texto en minúsculas
-    palindrome = palindrome.lower()
+    p = p.lower()
+
     # ¿Como podria hacer la comparacion de manera eficiente?
-    # Algunas ideas
-    # - Partir el string en dos y voltear la segunda mitad y comparar con la primera
-    # - Algun loop que compare el primer indice con el ultimo y vayan recorriendose
-    # Start coding here
-    pass
+    # Algunas ideas:
+    # 1. Partir el string en dos y voltear la segunda mitad y comparar con la primera
+    # 2. Algún loop que compare el primer indice con el ultimo y vayan recorriendose
+    n = len(p)
+    pivot = n // 2  # [1]
+    palindrome_found = True
+    i = 0
+
+    while palindrome_found and i < pivot:
+        print(f'Comparando {p[i]} con {p[n - i - 1]}')
+        if (p[i] != p[n - i - 1]):
+            # Si encontramos una letra que no es igual, no tiene caso seguir comparando
+            palindrome_found = False
+        i += 1
+
+    print(f'"{palindrome}" {"es" if palindrome_found else "no es"} un palíndromo')
+
+    return palindrome_found
 
 
 def validate():
