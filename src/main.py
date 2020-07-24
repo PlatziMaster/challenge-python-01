10f96df197cd63ceccbfbef06c8763f131157b4e
Julian Santos <ingjuliansantos@gmail.com>
2020-07-23 19:47:47 -0500

    Implemented is_palindrome function

--- src/main.py
+++ src/main.py
@@ -15,8 +15,27 @@ NOT_PALINDROMES = [
 
 def is_palindrome(palindrome):
     # Start coding here
-    pass
+    # I will develop this function with loops, it's too easy to do it with slices
 
+    #String preparation: to lowercase and delete the spaces
+    palindrome = palindrome.lower()
+    palindrome = palindrome.replace(' ', '')
+
+    #loop through each character of the string, comparing the first with the last, and so on.
+    str_len = len(palindrome)
+
+    i = 0
+    j = str_len - 1
+
+    while i < str_len:
+        if palindrome[i] != palindrome[j]:
+            return False
+        i += 1
+        j -= 1
+
+    return True
+    
+    
 def validate():
     for palindrome in PALINDROMES:
         if not is_palindrome(palindrome):
