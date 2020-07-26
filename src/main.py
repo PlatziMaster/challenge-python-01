371f65a6870f996bd27548a0b904485dcd0c3400
pachom <pachom@gmail.com>
2020-07-26 07:44:09 -0500

    challenge python 01 commit added by pachom

--- src/main.py
+++ src/main.py
@@ -14,8 +14,23 @@ NOT_PALINDROMES = [
 
 
 def is_palindrome(palindrome):
-    # Start coding here
-    pass
+    """
+    validates that the string palindrome is a 
+    palindrome (it is a word or phrase that 
+    read equal to right and back
+
+    param str palindrome is a word or Phrase
+
+    returns True if the word or phrase is a  
+    palindorme or False if not.
+    """
+    
+    reversed_letters = palindrome[::-1]
+
+    if reversed_letters.lower().replace(' ','') == palindrome.lower().replace(' ',''):
+        return True
+    else:
+        return False
 
 def validate():
     for palindrome in PALINDROMES:
