c1bfca713b02932854c37878b24d6643594b00a5
Juan Espitia <juanespitia@outlook.com>
2020-08-23 18:17:21 -0500

    palindromo resuelto

--- src/main.py
+++ src/main.py
@@ -15,6 +15,16 @@ NOT_PALINDROMES = [
 
 def is_palindrome(palindrome):
     # Start coding here
+    proof = palindrome.replace(" ","")
+    proof_low = proof.lower()
+    proof1 = list(proof_low)
+    proof2= list(proof_low)
+    proof2.reverse()
+    if proof1 == proof2:
+        return True
+    else:
+        return False
+
     pass
 
 def validate():
